85eb663820e8990655adb9179b20a7bcb13ebdbd
Ahmed Faisal Amit <ahmed.amit107@gmail.com>
2021-06-09 20:21:02 +0600

    linkedlist getFirst item and clear linkedlist function created

--- linked-list/linkedList.js
+++ linked-list/linkedList.js
@@ -10,9 +10,28 @@ class LinkedList {
     this.head = null;
   }
 
+  //   Insert data in the first position
   insetFirst(value) {
     const newNode = new Node(value, this.head);
     this.head = newNode;
+  }
+
+  //   Get the last element of the Linked List
+  getLast() {
+    let node = this.head;
+
+    while (node) {
+      if (node.next == null) {
+        return node.data;
+      }
+
+      node = node.next;
+    }
+  }
+
+  //   Clear the Linked List
+  clear() {
+    this.head = null;
   }
 }
 
@@ -21,4 +40,5 @@ const list = new LinkedList();
 list.insetFirst(10);
 list.insetFirst(20);
 list.insetFirst(30);
+list.clear();
 console.log(list);
