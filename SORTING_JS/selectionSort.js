6b160246eec83f1bc753061b5c7960e8b1baba4f
Ahmed Faisal Amit <ahmed.amit107@gmail.com>
2021-06-18 23:50:37 +0600

    selection sort added

---
+++ SORTING_JS/selectionSort.js
@@ -0,0 +1,21 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+
+    if (arr[i] !== arr[minIndex]) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([64, 34, 25, 12, 22, 11, 90]));
