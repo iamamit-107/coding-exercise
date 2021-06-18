eb4950b72b0a187464eaafa56549597ac4d9107a
Ahmed Faisal Amit <ahmed.amit107@gmail.com>
2021-06-18 23:37:56 +0600

    bubble sort added

---
+++ SORTING_JS/bubbleSort.js
@@ -0,0 +1,18 @@
+// Time Complexity: n*n
+// This is the easiest sorting implementation
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([64, 34, 25, 12, 22, 11, 90]));
